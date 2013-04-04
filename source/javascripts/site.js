a1a0a95c29ffaa1d644a5bdb8b38c2a903cff699
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-04-04 17:57:58 +0200

    new behaviour sliding from bottom up

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,28 +1,32 @@
 var menu;
 var arMenu;
+var sliding;
 
 $(document).ready(function() {
 	menu  = $("#menu ul");
+	sliding  = $("#sliding");
 	arMenu = menu.children("li");
 	arMenu.click(selectMenu);
+	
 	repositionMenuAtBottom();
  	$(window).resize(function() {
  		repositionMenuAtBottom();
 	});
-
+ 	$("#menu").scrollspy();
 
 });
 
 function repositionMenuAtBottom(){
 		verticalHeight = $(window).height();
-		menuHeight = menu.height() + parseInt(menu.css("border-top-width")) ;
-		bottomMenuPositon = verticalHeight - menuHeight;
-  		$("#sliding").css("top", bottomMenuPositon);
+  		sliding.css("margin-top", verticalHeight);
 	}
 
 function openMenu(){
 
 }
+
+
+
 
 
 $(function() {
