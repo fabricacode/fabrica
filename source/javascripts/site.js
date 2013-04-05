fc67522c0b386cda9095d74c6e94f6a569671692
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-04-05 13:07:02 +0200

    new image on cover and is touch detection

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,14 +2,23 @@ var menu;
 var arMenu;
 var sliding;
 var sections;
+var landing;
+var isTouch = Modernizr.touch;
+var BV;
+
 
 $(document).ready(function() {
 	menu  = $("#menu ul");
 	sliding  = $("#sliding");
 	sections  = $("#sliding > div");
+	landing =  $("#landing");
 	arMenu = menu.children("li");
-	 arMenu.click(selectMenu);
-	
+	arMenu.click(selectMenu);
+	landing.click(function(e){
+		e.preventDefault();
+      	BV.show('videos/fabrica_teaser_web_720.mp4',{ambient:true});
+      	console.log(e);
+	});
 	repositionMenuAtBottom();
 
  	$(window).resize(function() {
@@ -28,7 +37,7 @@ function openMenu(){
 }
 
 $(function() {
-    var BV = new $.BigVideo();
+    BV = new $.BigVideo({forceAutoplay:isTouch});
 	BV.init();
 	if (Modernizr.touch) {
 	    BV.show('images/cover.png');
