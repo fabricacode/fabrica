b8478a54efc57c42f516f0346f49d8a3283e534d
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-11 12:25:51 +0200

    about page with columns and maps responsive

---
+++ source/javascripts/other.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.gmap').mobileGmap();
+	$("#headline").fitText(1.0, { minFontSize: '14px', maxFontSize: '152px' });
+	$("#header-white").sticky({topSpacing:0});
+	
+ 	
+});
