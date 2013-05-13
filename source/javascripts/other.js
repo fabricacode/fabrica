6a4ce90a3596b2fd34cdea48be222387e6665ed7
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-13 11:08:41 +0200

    also about page

--- source/javascripts/other.js
+++ source/javascripts/other.js
@@ -1,3 +1,8 @@
+//= require  "vendor/jquery"
+//= require  "vendor/mobileGmap/jquery.mobilegmap.js"
+//= require  "vendor/FitText/jquery.fittext"
+//= require  "vendor/sticky/jquery.sticky"
+
 $(document).ready(function() {
 	$('.gmap').mobileGmap();
 	$("#headline").fitText(1.0, { minFontSize: '14px', maxFontSize: '152px' });
