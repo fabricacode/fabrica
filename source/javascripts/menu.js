16634260f027cdee2eb8b544d6c736f44dad3693
Aaron Siegel <datadreamerlabs@gmail.com>
2013-09-16 16:54:54 +0200

    Switched menus javascript links to be replaced onload for SEO.

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -1,5 +1,15 @@
 var visible = false;
 
+window.onload = function(event){
+	// look for all menu links and switch their HREF
+	$('.menulink').each(function(i){
+		var link = $(this).attr('href')
+		var newlink = "javascript:goto('" + link + "');";
+		$(this).attr('href', newlink);
+	});
+
+}
+
 function toggleMenu(){
 	if(visible){
 		hideMenu();
@@ -10,12 +20,14 @@ function toggleMenu(){
 
 function showMenu(){
 	// fade in bg
+	//$('body').css('overflow','hidden');
 	$('#menubg').fadeIn(300);
 	visible = true;
 }
 
 function hideMenu(){
 	// fade out bg
+	//$('body').css('overflow','scroll');
 	$('#menubg').fadeOut(300);
 	visible = false;
 }
