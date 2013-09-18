d0adcd68690b8f7d22dbb31c06cf3181442fa372
Aaron Siegel <datadreamerlabs@gmail.com>
2013-09-18 14:49:15 +0200

    More elegant menu transition solution.

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -1,14 +1,12 @@
 var visible = false;
 
-window.onload = function(event){
-	// look for all menu links and switch their HREF
-	$('.menulink').each(function(i){
-		var link = $(this).attr('href')
-		var newlink = "javascript:goto('" + link + "');";
-		$(this).attr('href', newlink);
+$(document).ready(function(){
+	$('.menulink').click(function(event){
+		event.preventDefault();
+		var link = this.href;
+		$('#menubg').fadeOut(300, function(){window.location = link});
 	});
-
-}
+});
 
 function toggleMenu(){
 	if(visible){
@@ -20,19 +18,12 @@ function toggleMenu(){
 
 function showMenu(){
 	// fade in bg
-	//$('body').css('overflow','hidden');
 	$('#menubg').fadeIn(300);
 	visible = true;
 }
 
 function hideMenu(){
 	// fade out bg
-	//$('body').css('overflow','scroll');
 	$('#menubg').fadeOut(300);
 	visible = false;
-}
-
-function goto(link){
-	toggleMenu();
-	setTimeout(function(){window.location = link}, 300);
 }
