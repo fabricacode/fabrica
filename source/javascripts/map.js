045629aa9b778a7c54237f0eb8a433732bfd738f
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-09-23 15:43:19 +0200

    missing 1 fix

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -6,7 +6,7 @@
 $(document).ready(function() {
 	$('.gmap').mobileGmap();
 	$("#headline").fitText(1.0, { minFontSize: '14px', maxFontSize: '152px' });
-	$("#header-white").sticky({topSpacing:0});
+	$("#header").sticky({topSpacing:0});
 	
  	
 });
